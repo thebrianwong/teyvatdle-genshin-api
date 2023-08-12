20fa14a5563567836a2e26953e870d2b4531758b
Brian Wong <brianw7218@gmail.com>
2023-08-11 23:06:35 -0700

    Flesh out region test

--- src/tests/region.test.ts
+++ src/tests/region.test.ts
@@ -16,7 +16,37 @@ test("return Regions as JSON", (done) => {
     .expect("Content-Type", /json/)
     .expect(200)
     .expect((res) => {
+      const [mondstadt, liyue, inazuma, sumeru, fontaine, natlan, snezhnaya] =
+        res.body;
       expect(res.body).toBeDefined();
+      expect(mondstadt).toMatchObject({
+        id: 1,
+        name: "Mondstadt",
+      });
+      expect(liyue).toMatchObject({
+        id: 2,
+        name: "Liyue",
+      });
+      expect(inazuma).toMatchObject({
+        id: 3,
+        name: "Inazuma",
+      });
+      expect(sumeru).toMatchObject({
+        id: 4,
+        name: "Sumeru",
+      });
+      expect(fontaine).toMatchObject({
+        id: 5,
+        name: "Fontaine",
+      });
+      expect(natlan).toMatchObject({
+        id: 6,
+        name: "Natlan",
+      });
+      expect(snezhnaya).toMatchObject({
+        id: 7,
+        name: "Snezhnaya",
+      });
     })
     .end(done);
 });
