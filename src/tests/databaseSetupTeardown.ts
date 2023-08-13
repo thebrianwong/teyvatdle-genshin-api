5b777ca9feba1f3fc0fef808fae70163ecc66fcc
Brian Wong <brianw7218@gmail.com>
2023-08-12 23:24:29 -0700

    Adjust test config logging messages

--- src/tests/databaseSetupTeardown.ts
+++ src/tests/databaseSetupTeardown.ts
@@ -8,10 +8,13 @@ const configSetup = async (entity: string) => {
 const configTeardown = async (entity: string) => {
   await AppDataSource.destroy();
   console.log(
-    `Disconnected from the test Docker database for ${entity} testing.`
+    `Disconnected from the test Docker database at the end of ${entity} testing.`
   );
 
   server.close();
+  console.log(
+    `Disconnected from the HTTP server at the end of ${entity} testing.`
+  );
 };
 
 export { configSetup, configTeardown };
