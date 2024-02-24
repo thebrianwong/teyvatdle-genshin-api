9a7fadc2429b8569eaa3c963047a8c5a1c037255
Brian Wong <brianw7218@gmail.com>
2024-02-23 23:26:22 -0800

    Start trying to figure out resolver functions

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,22 +1,43 @@
 import { GraphQLResolveInfo } from "graphql";
 import {
-  GameData,
+  // GameData,
   Maybe,
   Resolver,
   ResolverTypeWrapper,
   Resolvers,
 } from "../generated/graphql";
 import { getGameData } from "../controllers/teyvatdleGameDataController";
+import {
+  getCharacters,
+  retrieveCharacterData,
+  testForCharId,
+} from "../controllers/characterController";
 // import { getGameData } from "../controllers/teyvatdleGameDataController";
 
 const resolvers: Resolvers = {
   Query: {
-    gameData: (
-      parent: any,
-      args: any,
-      contextValue: any,
-      info: GraphQLResolveInfo
-    ) => getGameData(),
+    characterData: () =>
+      // parent: any,
+      // args: any,
+      // contextValue: any,
+      // info: GraphQLResolveInfo
+      retrieveCharacterData(),
+  },
+  CharacterData: {
+    // characterId: () => testForCharId(),
+    characterId: (parent) => {
+      console.log(parent);
+      return parent.element;
+    },
+    // characterId:
+    //   ()  => {
+    //     return {characterId: "d"}
+    //   }
+    gender: (parent) => parent.gender,
+    // height: (parent) => parent.height,
+    // region: (parent) => parent.region,
+    element: (parent) => parent.element,
+    birthday: (parent) => parent.birthday,
   },
 };
 
