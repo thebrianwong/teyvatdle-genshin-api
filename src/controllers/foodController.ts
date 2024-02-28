fe896cc206632579afa80039d25e65b80a32f817
Brian Wong <brianw7218@gmail.com>
2024-02-27 21:35:58 -0800

    Change food object keys to match GraphQL schema

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from "express";
 import { AppDataSource } from "../index";
 import Food from "../models/food.model";
-import FoodData from "../types/data/foodData.type";
+// import FoodData from "../types/data/foodData.type";
+import { FoodData } from "../generated/graphql";
 
 const retrieveFoodData: () => Promise<FoodData[]> = async () => {
   const foodRepo = AppDataSource.getRepository(Food);
@@ -10,17 +11,17 @@ const retrieveFoodData: () => Promise<FoodData[]> = async () => {
       .createQueryBuilder("food")
       .innerJoin("food.typeId", "food_type")
       .select([
-        "food.id AS food_id",
-        "food.name AS food_name",
+        'food.id AS "foodId"',
+        'food.name AS "foodName"',
         "food.rarity AS rarity",
-        "food_type.name AS food_type",
-        "food.specialDish AS special_dish",
+        'food_type.name AS "foodType"',
+        'food.specialDish AS "specialDish"',
         "food.purchasable AS purchasable",
         "food.recipe AS recipe",
         "food.event AS event",
-        "food.imageUrl AS food_image_url",
+        'food.imageUrl AS "foodImageUrl"',
       ])
-      .orderBy({ food_name: "ASC" })
+      .orderBy({ '"foodName"': "ASC" })
       .getRawMany();
     return foods;
   } catch (err) {
