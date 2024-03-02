a74427f87c79cd29759cf858012a88ede2981332
Brian Wong <brianw7218@gmail.com>
2024-03-01 23:14:35 -0800

    Organize daily record query root resolver into its own file

--- src/resolvers/queries/query.ts
+++ src/resolvers/queries/query.ts
@@ -1,11 +1,10 @@
-import { GraphQLResolveInfo } from "graphql";
 import { QueryResolvers } from "../../generated/graphql";
-import { getDailyRecord } from "../../controllers/teyvatdleGameDataController";
 import characterDataRootResolvers from "./root/characterData";
 import constellationDataRootResolvers from "./root/constellationData";
 import talentDataRootResolvers from "./root/talentData";
 import foodDataRootResolvers from "./root/foodData";
 import weaponDataRootResolvers from "./root/weaponData";
+import dailyRecordDataRootResolvers from "./root/dailyRecord";
 
 const queryResolvers: QueryResolvers<any, {}> = {
   characterData: characterDataRootResolvers,
@@ -13,12 +12,7 @@ const queryResolvers: QueryResolvers<any, {}> = {
   foodData: foodDataRootResolvers,
   talentData: talentDataRootResolvers,
   weaponData: weaponDataRootResolvers,
-  dailyRecordData: (
-    parent: any,
-    args: any,
-    contextValue: any,
-    info: GraphQLResolveInfo
-  ) => getDailyRecord(),
+  dailyRecordData: dailyRecordDataRootResolvers,
 };
 
 export default queryResolvers;
