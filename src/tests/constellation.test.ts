c6f7aeddf2125cc495d4ae67f408c0bc7b8acc3f
Brian Wong <brianw7218@gmail.com>
2024-03-06 22:02:26 -0800

    Add tests for constellation queries with arguments

--- src/tests/constellation.test.ts
+++ src/tests/constellation.test.ts
@@ -91,3 +91,417 @@ test("return the correct number of Constellations", (done) => {
     })
     .end(done);
 });
+
+describe("Constellation query argument test suite", () => {
+  test("a null filter argument returns an error", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: null) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const data = response.data;
+        const errors = response.errors;
+
+        expect(data).toBeNull();
+        expect(errors[0].message).toBe("Please enter a filter value.");
+      })
+      .end(done);
+  });
+
+  test("if multiple null arguments are provided, return an error", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(
+          filter: { id: null, constellationName: null, characterName: null, random: null }
+        ) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const data = response.data;
+        const errors = response.errors;
+
+        expect(data).toBeNull();
+        expect(errors[0].message).toBe("Please enter a single filter value.");
+      })
+      .end(done);
+  });
+
+  test("if multiple valid arguments are provided, return an error", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(
+          filter: { id: 1, constellationName: "One Arrow to Rule Them All", characterName: "Amber", random: true }
+        ) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const data = response.data;
+        const errors = response.errors;
+
+        expect(data).toBeNull();
+        expect(errors[0].message).toBe("Please enter a single filter value.");
+      })
+      .end(done);
+  });
+
+  describe("a null argument returns an error", () => {
+    test("id", (done) => {
+      const queryData = {
+        query: `query ConstellationData {
+          constellationData(filter: { id: null }) {
+            constellationId
+          }
+        }`,
+      };
+
+      request(app)
+        .post("/graphql")
+        .send(queryData)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect((res) => {
+          const response = res.body;
+          const data = response.data;
+          const errors = response.errors;
+
+          expect(data).toBeNull();
+          expect(errors[0].message).toBe(
+            "Invalid argument. Please enter an id."
+          );
+        })
+        .end(done);
+    });
+
+    test("constellationName", (done) => {
+      const queryData = {
+        query: `query ConstellationData {
+          constellationData(filter: { constellationName: null }) {
+            constellationId
+          }
+        }
+        `,
+      };
+
+      request(app)
+        .post("/graphql")
+        .send(queryData)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect((res) => {
+          const response = res.body;
+          const data = response.data;
+          const errors = response.errors;
+
+          expect(data).toBeNull();
+          expect(errors[0].message).toBe(
+            "Invalid argument. Please enter a constellation name."
+          );
+        })
+        .end(done);
+    });
+
+    test("characterName", (done) => {
+      const queryData = {
+        query: `query ConstellationData {
+          constellationData(filter: { characterName: null }) {
+            constellationId
+          }
+        }`,
+      };
+
+      request(app)
+        .post("/graphql")
+        .send(queryData)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect((res) => {
+          const response = res.body;
+          const data = response.data;
+          const errors = response.errors;
+
+          expect(data).toBeNull();
+          expect(errors[0].message).toBe(
+            "Invalid argument. Please enter a character name."
+          );
+        })
+        .end(done);
+    });
+
+    test("random", (done) => {
+      const queryData = {
+        query: `query ConstellationData {
+          constellationData(filter: { random: null }) {
+            constellationId
+          }
+        }`,
+      };
+
+      request(app)
+        .post("/graphql")
+        .send(queryData)
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .expect((res) => {
+          const response = res.body;
+          const data = response.data;
+          const errors = response.errors;
+
+          expect(data).toBeNull();
+          expect(errors[0].message).toBe(
+            'Invalid argument. Please set the argument "random" to "true" or "false".'
+          );
+        })
+        .end(done);
+    });
+  });
+
+  test("a non-number id returns an error", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { id: "hello" }) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const data = response.data;
+        const errors = response.errors;
+
+        expect(data).toBeNull();
+        expect(errors[0].message).toBe(
+          "Invalid argument. Please enter a number."
+        );
+      })
+      .end(done);
+  });
+
+  test("if the Constellation ID exists, return the Constellation", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { id: "1" }) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const constellation = response.data.constellationData[0];
+
+        expect(constellation).toHaveProperty("constellationId", "1");
+      })
+      .end(done);
+  });
+
+  test("if the Constellation ID does not exist, return an empty array", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { id: "14347" }) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const emptyArray = response.data.constellationData;
+
+        expect(emptyArray.length).toBe(0);
+      })
+      .end(done);
+  });
+
+  test("if the Constellation name exists, return the Constellation", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { constellationName: "One Arrow to Rule Them All" }) {
+          constellationName
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const constellation = response.data.constellationData[0];
+
+        expect(constellation).toHaveProperty(
+          "constellationName",
+          "One Arrow to Rule Them All"
+        );
+      })
+      .end(done);
+  });
+
+  test("if the Constellation name does not exist, return an empty array", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { constellationName: "Very Real Name" }) {
+          constellationName
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const emptyArray = response.data.constellationData;
+
+        expect(emptyArray.length).toBe(0);
+      })
+      .end(done);
+  });
+
+  test("if the Character name exists, return the Character", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { characterName: "Amber" }) {
+          characterName
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const constellation = response.data.constellationData[0];
+
+        expect(constellation).toHaveProperty("characterName", "Amber");
+      })
+      .end(done);
+  });
+
+  test("if the Character name does not exist, return an empty array", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { characterName: "Paimon" }) {
+          characterName
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const emptyArray = response.data.constellationData;
+
+        expect(emptyArray.length).toBe(0);
+      })
+      .end(done);
+  });
+
+  test("if the random argument is set to true, return a random character", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { random: true }) {
+          constellationName
+        }
+      }`,
+    };
+
+    const mathRandomSpy = jest.spyOn(Math, "random").mockImplementation(() => {
+      return 0;
+    });
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const constellation = response.data.constellationData[0];
+
+        expect(constellation).toHaveProperty(
+          "constellationName",
+          "One Arrow to Rule Them All"
+        );
+        mathRandomSpy.mockRestore();
+      })
+      .end(done);
+  });
+
+  test("if the random argument is set to false, return character data as if no argument was provided", (done) => {
+    const queryData = {
+      query: `query ConstellationData {
+        constellationData(filter: { random: false }) {
+          constellationId
+        }
+      }`,
+    };
+
+    request(app)
+      .post("/graphql")
+      .send(queryData)
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        const response = res.body;
+        const constellations = response.data.constellationData;
+
+        expect(constellations.length).toBeGreaterThan(1);
+      })
+      .end(done);
+  });
+});
