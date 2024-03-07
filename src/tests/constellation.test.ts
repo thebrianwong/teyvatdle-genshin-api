bfd588ca6802f74dae4e22ef370634681e93d8e4
Brian Wong <brianw7218@gmail.com>
2024-03-06 21:40:39 -0800

    Update constellation tests for GraphQL

--- src/tests/constellation.test.ts
+++ src/tests/constellation.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest";
 import { app } from "../index";
 import { configSetup, configTeardown } from "./databaseSetupTeardown";
-import ConstellationData from "../types/data/constellationData.type";
+import { ConstellationData } from "../generated/graphql";
 
 beforeAll(async () => {
   await configSetup("Constellation");
@@ -11,9 +11,23 @@ afterAll(async () => {
   await configTeardown("Constellation");
 });
 
+const queryData = {
+  query: `query ConstellationData {
+    constellationData {
+      constellationId
+      constellationName
+      constellationLevel
+      constellationImageUrl
+      characterName
+      characterImageUrl
+    }
+  }`,
+};
+
 test("return Constellations as JSON", (done) => {
   request(app)
-    .get("/api/constellation")
+    .post("/graphql")
+    .send(queryData)
     .expect("Content-Type", /json/)
     .expect(200)
     .end(done);
@@ -21,19 +35,21 @@ test("return Constellations as JSON", (done) => {
 
 test("expect none of the Constellation keys/columns are null", (done) => {
   request(app)
-    .get("/api/constellation")
+    .post("/graphql")
+    .send(queryData)
     .expect("Content-Type", /json/)
     .expect(200)
     .expect((res) => {
-      const arrayOfDataObjects: ConstellationData[] = res.body;
+      const arrayOfDataObjects: ConstellationData[] =
+        res.body.data.constellationData;
       expect(arrayOfDataObjects).toEqual(
         expect.arrayContaining([
           expect.objectContaining({
-            constellation_name: expect.anything(),
-            constellation_level: expect.anything(),
-            constellation_image_url: expect.anything(),
-            character_name: expect.anything(),
-            character_image_url: expect.anything(),
+            constellationName: expect.anything(),
+            constellationLevel: expect.anything(),
+            constellationImageUrl: expect.anything(),
+            characterName: expect.anything(),
+            characterImageUrl: expect.anything(),
           }),
         ])
       );
@@ -43,17 +59,19 @@ test("expect none of the Constellation keys/columns are null", (done) => {
 
 test("returned Constellation data has the correct types for values", (done) => {
   request(app)
-    .get("/api/constellation")
+    .post("/graphql")
+    .send(queryData)
     .expect("Content-Type", /json/)
     .expect(200)
     .expect((res) => {
-      const arrayOfDataObjects: ConstellationData[] = res.body;
+      const arrayOfDataObjects: ConstellationData[] =
+        res.body.data.constellationData;
       arrayOfDataObjects.forEach((data) => {
-        expect(typeof data.constellation_name).toBe("string");
-        expect(typeof data.constellation_level).toBe("number");
-        expect(typeof data.constellation_image_url).toBe("string");
-        expect(typeof data.character_name).toBe("string");
-        expect(typeof data.character_image_url).toBe("string");
+        expect(typeof data.constellationName).toBe("string");
+        expect(typeof data.constellationLevel).toBe("number");
+        expect(typeof data.constellationImageUrl).toBe("string");
+        expect(typeof data.characterName).toBe("string");
+        expect(typeof data.characterImageUrl).toBe("string");
       });
     })
     .end(done);
@@ -62,11 +80,13 @@ test("returned Constellation data has the correct types for values", (done) => {
 test("return the correct number of Constellations", (done) => {
   const numOfConstellations = 426;
   request(app)
-    .get("/api/constellation")
+    .post("/graphql")
+    .send(queryData)
     .expect("Content-Type", /json/)
     .expect(200)
     .expect((res) => {
-      const arrayOfDataObjects: ConstellationData[] = res.body;
+      const arrayOfDataObjects: ConstellationData[] =
+        res.body.data.constellationData;
       expect(arrayOfDataObjects).toHaveLength(numOfConstellations);
     })
     .end(done);
