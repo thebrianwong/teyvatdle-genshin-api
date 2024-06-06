622c8a9ecc7d4f62861b59c3d58c92b6cf580aa5
Brian Wong <brianw7218@gmail.com>
2024-06-05 21:54:31 -0700

    Add weapons key function

--- src/redis/keys.ts
+++ src/redis/keys.ts
@@ -4,4 +4,6 @@ const charactersKey = () => `${APP_KEY_PREFIX}#characters`; // key for JSON data
 const characterByIdKey = () => `${APP_KEY_PREFIX}:character#byId`; // key for JSON data structure, returns character JSON
 const characterNameToIdKey = () => `${APP_KEY_PREFIX}:character#nameToId`; // key for Hash data structure, returns character ID to be passed into characterIdKey
 
-export { charactersKey, characterByIdKey, characterNameToIdKey };
+const weaponsKey = () => `${APP_KEY_PREFIX}#weapons`; // key for JSON data structure
+
+export { charactersKey, characterByIdKey, characterNameToIdKey, weaponsKey };
