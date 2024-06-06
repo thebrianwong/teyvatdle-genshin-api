ddcd69bbcd0dd712b72a1fa20e60c98a64376772
Brian Wong <brianw7218@gmail.com>
2024-06-06 12:10:14 -0700

    Fix weapon key values

--- src/redis/keys.ts
+++ src/redis/keys.ts
@@ -5,8 +5,8 @@ const characterByIdKey = () => `${APP_KEY_PREFIX}:character#byId`; // key for JS
 const characterNameToIdKey = () => `${APP_KEY_PREFIX}:character#nameToId`; // key for Hash data structure, returns character ID to be passed into characterIdKey
 
 const weaponsKey = () => `${APP_KEY_PREFIX}#weapons`; // key for JSON data structure
-const weaponByIdKey = () => `${APP_KEY_PREFIX}:character#byId`; // key for JSON data structure, returns weapon JSON
-const weaponNameToIdKey = () => `${APP_KEY_PREFIX}:character#nameToId`; // key for Hash data structure, returns weapon ID to be passed into weaponIdKey
+const weaponByIdKey = () => `${APP_KEY_PREFIX}:weapon#byId`; // key for JSON data structure, returns weapon JSON
+const weaponNameToIdKey = () => `${APP_KEY_PREFIX}:weapon#nameToId`; // key for Hash data structure, returns weapon ID to be passed into weaponIdKey
 
 export {
   charactersKey,
