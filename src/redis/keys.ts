5af51a8430829f5f37472df35c1076b823c19e93
Brian Wong <brianw7218@gmail.com>
2024-06-05 21:28:53 -0700

    Add more character keys and comments

--- src/redis/keys.ts
+++ src/redis/keys.ts
@@ -1,5 +1,7 @@
 const APP_KEY_PREFIX = "TEYVATDLE";
 
-const charactersKey = () => `${APP_KEY_PREFIX}#characters`;
+const charactersKey = () => `${APP_KEY_PREFIX}#characters`; // key for JSON data structure
+const characterByIdKey = () => `${APP_KEY_PREFIX}:character#byId`; // key for JSON data structure, returns character JSON
+const characterNameToIdKey = () => `${APP_KEY_PREFIX}:character#nameToId`; // key for Hash data structure, returns character ID to be passed into characterIdKey
 
-export { charactersKey };
+export { charactersKey, characterByIdKey, characterNameToIdKey };
