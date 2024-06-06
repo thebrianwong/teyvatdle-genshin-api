5d54240631402d66c21568b440120a587253f8ba
Brian Wong <brianw7218@gmail.com>
2024-06-05 21:55:05 -0700

    Add caching for all weapons

--- src/controllers/weaponController.ts
+++ src/controllers/weaponController.ts
@@ -1,35 +1,46 @@
-import { AppDataSource } from "../index";
+import { AppDataSource, client } from "../index";
 import Weapon from "../models/weapon.model";
 import { WeaponData } from "../generated/graphql";
+import { weaponsKey } from "../redis/keys";
+import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
 
 const retrieveWeaponData: () => Promise<WeaponData[]> = async () => {
   const weaponRepo = AppDataSource.getRepository(Weapon);
   try {
-    const weapons: WeaponData[] = await weaponRepo
-      .createQueryBuilder("weapon")
-      .innerJoin("weapon.typeId", "weapon_type")
-      .leftJoin("weapon.subStatId", "stat")
-      .innerJoin("weapon.weaponDomainMaterialId", "weapon_domain_material")
-      .innerJoin("weapon.eliteEnemyMaterialId", "elite_enemy_drop")
-      .innerJoin("weapon.commonEnemyMaterialId", "common_enemy_drop")
-      .select([
-        'weapon.id AS "weaponId"',
-        'weapon.name AS "weaponName"',
-        "weapon.rarity AS rarity",
-        'weapon_type.name AS "weaponType"',
-        'stat.name AS "subStat"',
-        'weapon.imageUrl AS "weaponImageUrl"',
-        'weapon_domain_material.name AS "weaponDomainMaterial"',
-        'weapon_domain_material.imageUrl AS "weaponDomainMaterialImageUrl"',
-        'elite_enemy_drop.name AS "eliteEnemyMaterial"',
-        'elite_enemy_drop.imageUrl AS "eliteEnemyMaterialImageUrl"',
-        'common_enemy_drop.name AS "commonEnemyMaterial"',
-        'common_enemy_drop.imageUrl AS "commonEnemyMaterialImageUrl"',
-        "weapon.gacha AS gacha",
-      ])
-      .orderBy({ '"weaponName"': "ASC" })
-      .getRawMany();
-    return weapons;
+    const cachedWeapons = await client.json.get(weaponsKey());
+    if (cachedWeapons) {
+      return cachedWeapons as WeaponData[];
+    } else {
+      const weapons: WeaponData[] = await weaponRepo
+        .createQueryBuilder("weapon")
+        .innerJoin("weapon.typeId", "weapon_type")
+        .leftJoin("weapon.subStatId", "stat")
+        .innerJoin("weapon.weaponDomainMaterialId", "weapon_domain_material")
+        .innerJoin("weapon.eliteEnemyMaterialId", "elite_enemy_drop")
+        .innerJoin("weapon.commonEnemyMaterialId", "common_enemy_drop")
+        .select([
+          'weapon.id AS "weaponId"',
+          'weapon.name AS "weaponName"',
+          "weapon.rarity AS rarity",
+          'weapon_type.name AS "weaponType"',
+          'stat.name AS "subStat"',
+          'weapon.imageUrl AS "weaponImageUrl"',
+          'weapon_domain_material.name AS "weaponDomainMaterial"',
+          'weapon_domain_material.imageUrl AS "weaponDomainMaterialImageUrl"',
+          'elite_enemy_drop.name AS "eliteEnemyMaterial"',
+          'elite_enemy_drop.imageUrl AS "eliteEnemyMaterialImageUrl"',
+          'common_enemy_drop.name AS "commonEnemyMaterial"',
+          'common_enemy_drop.imageUrl AS "commonEnemyMaterialImageUrl"',
+          "weapon.gacha AS gacha",
+        ])
+        .orderBy({ '"weaponName"': "ASC" })
+        .getRawMany();
+      await Promise.all([
+        await client.json.set(weaponsKey(), "$", weapons),
+        await client.expireAt(weaponsKey(), expireKeyTomorrow(), "NX"),
+      ]);
+      return weapons;
+    }
   } catch (err) {
     throw new Error("There was an error querying weapons.");
   }
