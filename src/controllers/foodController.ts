6fcea6aef2dd44d81953121ccd3a7206245e4042
Brian Wong <brianw7218@gmail.com>
2024-06-06 12:30:20 -0700

    Add caching to all foods

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,27 +1,38 @@
-import { AppDataSource } from "../index";
+import { AppDataSource, client } from "../index";
 import Food from "../models/food.model";
 import { FoodData } from "../generated/graphql";
+import { foodsKey } from "../redis/keys";
+import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
 
 const retrieveFoodData: () => Promise<FoodData[]> = async () => {
-  const foodRepo = AppDataSource.getRepository(Food);
   try {
-    const foods: FoodData[] = await foodRepo
-      .createQueryBuilder("food")
-      .innerJoin("food.typeId", "food_type")
-      .select([
-        'food.id AS "foodId"',
-        'food.name AS "foodName"',
-        "food.rarity AS rarity",
-        'food_type.name AS "foodType"',
-        'food.specialDish AS "specialDish"',
-        "food.purchasable AS purchasable",
-        "food.recipe AS recipe",
-        "food.event AS event",
-        'food.imageUrl AS "foodImageUrl"',
-      ])
-      .orderBy({ '"foodName"': "ASC" })
-      .getRawMany();
-    return foods;
+    const cachedFoods = await client.json.get(foodsKey());
+    if (cachedFoods) {
+      return cachedFoods as FoodData[];
+    } else {
+      const foodRepo = AppDataSource.getRepository(Food);
+      const foods: FoodData[] = await foodRepo
+        .createQueryBuilder("food")
+        .innerJoin("food.typeId", "food_type")
+        .select([
+          'food.id AS "foodId"',
+          'food.name AS "foodName"',
+          "food.rarity AS rarity",
+          'food_type.name AS "foodType"',
+          'food.specialDish AS "specialDish"',
+          "food.purchasable AS purchasable",
+          "food.recipe AS recipe",
+          "food.event AS event",
+          'food.imageUrl AS "foodImageUrl"',
+        ])
+        .orderBy({ '"foodName"': "ASC" })
+        .getRawMany();
+      await Promise.all([
+        client.json.set(foodsKey(), "$", foods),
+        client.expireAt(foodsKey(), expireKeyTomorrow(), "NX"),
+      ]);
+      return foods;
+    }
   } catch (err) {
     throw new Error("There was an error querying foods.");
   }
