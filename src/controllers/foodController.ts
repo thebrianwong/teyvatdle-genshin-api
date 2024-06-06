b37a90f033e9a360c76ef5f487cc148802474cfd
Brian Wong <brianw7218@gmail.com>
2024-06-06 14:16:41 -0700

    Add caching for food search

--- src/controllers/foodController.ts
+++ src/controllers/foodController.ts
@@ -1,7 +1,13 @@
 import { AppDataSource, client } from "../index";
 import Food from "../models/food.model";
-import { FoodData } from "../generated/graphql";
-import { foodsKey } from "../redis/keys";
+import { FoodData, FoodType } from "../generated/graphql";
+import {
+  foodByIdKey,
+  foodNameToIdKey,
+  foodsByTypeKey,
+  foodsKey,
+  formatAsKey,
+} from "../redis/keys";
 import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
 
 const retrieveFoodData: () => Promise<FoodData[]> = async () => {
@@ -40,76 +46,95 @@ const retrieveFoodData: () => Promise<FoodData[]> = async () => {
 
 const retrieveFilteredFoodData: (
   filterType: "id" | "foodName" | "foodType",
-  searchValue: String
+  searchValue: string | FoodType
 ) => Promise<FoodData[]> = async (filterType, searchValue) => {
-  const foodRepo = AppDataSource.getRepository(Food);
   try {
-    const baseQuery = foodRepo
-      .createQueryBuilder("food")
-      .innerJoin("food.typeId", "food_type")
-      .select([
-        'food.id AS "foodId"',
-        'food.name AS "foodName"',
-        "food.rarity AS rarity",
-        'food_type.name AS "foodType"',
-        'food.specialDish AS "specialDish"',
-        "food.purchasable AS purchasable",
-        "food.recipe AS recipe",
-        "food.event AS event",
-        'food.imageUrl AS "foodImageUrl"',
-      ]);
-    if (filterType === "id") {
-      baseQuery.where("food.id = :id", { id: Number(searchValue) });
-    } else if (filterType === "foodName") {
-      baseQuery.where("food.name = :foodName", {
-        foodName: searchValue,
-      });
-    } else if (filterType === "foodType") {
-      baseQuery.where("food_type.name = :foodType", {
-        foodType: searchValue,
-      });
+    let foodCacheKey: string | undefined = searchValue;
+    if (filterType === "foodName") {
+      foodCacheKey = await client.hGet(foodNameToIdKey(), searchValue);
     }
-    const foods: FoodData[] = await baseQuery
-      .orderBy({ '"foodId"': "ASC" })
-      .getRawMany();
-    if (foods.length > 0) {
-      if (foods.length === 1) {
-        // id or name
-        const foodId = foods[0].foodId!.toString();
-        const foodName = foods[0].foodName!;
-        await Promise.all([
-          client.json.set(foodByIdKey(), "$", {}, { NX: true }),
-          client.json.set(foodByIdKey(), foodId, foods, {
-            NX: true,
-          }),
-          // client.expireAt(foodByIdKey(), expireKeyTomorrow(), "NX"),
-          client.expire(foodByIdKey(), 10, "NX"),
-          client.hSet(foodNameToIdKey(), foodName, foodId),
-          // client.expireAt(foodNameToIdKey(), expireKeyTomorrow(), "NX"),
-          client.expire(foodNameToIdKey(), 10, "NX"),
-        ]);
+
+    let foodCacheKeyExists;
+    if (filterType === "foodType") {
+      foodCacheKeyExists = await client.json.type(
+        foodsByTypeKey(),
+        // foodType keys have to be formatted due to containing apostrophes and white space
+        formatAsKey(foodCacheKey!)
+      );
+    } else {
+      foodCacheKeyExists = await client.json.type(foodByIdKey(), foodCacheKey);
+    }
+
+    if (foodCacheKey && foodCacheKeyExists) {
+      if (filterType === "foodType") {
+        const foods = (await client.json.get(foodsByTypeKey(), {
+          path: formatAsKey(foodCacheKey),
+        })) as FoodData[];
+        return foods;
       } else {
-        // food type search
-        const foodType = foods[0].foodType!;
-        console.log(foodType);
-        await Promise.all([
-          client.json.set(foodsByTypeKey(), "$", {}, { NX: true }),
-          // client.json.set(foodsByTypeKey(), `"${foodType.toString()}"`, foods, {
-          client.json.set(
-            foodsByTypeKey(),
-            foodType.replaceAll("'", `\'`),
-            foods,
-            {
-              // client.json.set(foodsByTypeKey(), foodType.toString(), foods, {
-              NX: true,
-            }
-          ),
-          // client.expireAt(foodsByTypeKey(), expireKeyTomorrow(), "NX"),
-          client.expire(foodsByTypeKey(), 10, "NX"),
-        ]);
+        const foods = (await client.json.get(foodByIdKey(), {
+          path: foodCacheKey,
+        })) as FoodData[];
+        return foods;
       }
+    } else {
+      const foodRepo = AppDataSource.getRepository(Food);
+      const baseQuery = foodRepo
+        .createQueryBuilder("food")
+        .innerJoin("food.typeId", "food_type")
+        .select([
+          'food.id AS "foodId"',
+          'food.name AS "foodName"',
+          "food.rarity AS rarity",
+          'food_type.name AS "foodType"',
+          'food.specialDish AS "specialDish"',
+          "food.purchasable AS purchasable",
+          "food.recipe AS recipe",
+          "food.event AS event",
+          'food.imageUrl AS "foodImageUrl"',
+        ]);
+      if (filterType === "id") {
+        baseQuery.where("food.id = :id", { id: Number(searchValue) });
+      } else if (filterType === "foodName") {
+        baseQuery.where("food.name = :foodName", {
+          foodName: searchValue,
+        });
+      } else if (filterType === "foodType") {
+        baseQuery.where("food_type.name = :foodType", {
+          foodType: searchValue,
+        });
+      }
+      const foods: FoodData[] = await baseQuery
+        .orderBy({ '"foodId"': "ASC" })
+        .getRawMany();
+      if (foods.length > 0) {
+        if (foods.length === 1) {
+          // id or name
+          const foodId = foods[0].foodId!.toString();
+          const foodName = foods[0].foodName!;
+          await Promise.all([
+            client.json.set(foodByIdKey(), "$", {}, { NX: true }),
+            client.json.set(foodByIdKey(), foodId, foods, {
+              NX: true,
+            }),
+            client.expireAt(foodByIdKey(), expireKeyTomorrow(), "NX"),
+            client.hSet(foodNameToIdKey(), foodName, foodId),
+            client.expireAt(foodNameToIdKey(), expireKeyTomorrow(), "NX"),
+          ]);
+        } else {
+          // food type search
+          const foodType = foods[0].foodType!;
+          await Promise.all([
+            client.json.set(foodsByTypeKey(), "$", {}, { NX: true }),
+            client.json.set(foodsByTypeKey(), formatAsKey(foodType), foods, {
+              NX: true,
+            }),
+            client.expireAt(foodsByTypeKey(), expireKeyTomorrow(), "NX"),
+          ]);
+        }
+      }
+      return foods;
     }
-    return foods;
   } catch (err) {
     throw new Error("There was an error querying foods.");
   }
