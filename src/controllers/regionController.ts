9e044b9f47c79d73769eb430e376c58fdd836146
Brian Wong <brianw7218@gmail.com>
2024-06-07 11:41:59 -0700

    Add caching for regions

--- src/controllers/regionController.ts
+++ src/controllers/regionController.ts
@@ -1,15 +1,27 @@
 import { AppDataSource } from "../index";
 import Region from "../models/lookups/region.model";
 import { RegionData } from "../generated/graphql";
+import client from "../redis/client";
+import { regionsKey } from "../redis/keys";
+import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
 
 const getRegions: () => Promise<RegionData[]> = async () => {
-  const regionRepo = AppDataSource.getRepository(Region);
   try {
-    const regions: RegionData[] = await regionRepo
-      .createQueryBuilder("region")
-      .select(["region.id AS id", "region.name AS name"])
-      .getRawMany();
-    return regions;
+    const cachedRegions = await client.json.get(regionsKey());
+    if (cachedRegions) {
+      return cachedRegions as RegionData[];
+    } else {
+      const regionRepo = AppDataSource.getRepository(Region);
+      const regions: RegionData[] = await regionRepo
+        .createQueryBuilder("region")
+        .select(["region.id AS id", "region.name AS name"])
+        .getRawMany();
+      await Promise.all([
+        client.json.set(regionsKey(), "$", regions),
+        client.expireAt(regionsKey(), expireKeyTomorrow(), "NX"),
+      ]);
+      return regions;
+    }
   } catch (err) {
     throw new Error("There was an error querying regions.");
   }
