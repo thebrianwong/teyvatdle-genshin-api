4eaa2eae63318f1e8cd6e87f59bfaa4b6ee4edd2
Brian Wong <brianw7218@gmail.com>
2024-06-07 11:46:23 -0700

    Add caching to local specialties

--- src/controllers/localSpecialtyController.ts
+++ src/controllers/localSpecialtyController.ts
@@ -1,20 +1,32 @@
 import { AppDataSource } from "../index";
 import LocalSpecialty from "../models/localSpecialty.model";
 import { LocalSpecialtyData } from "../generated/graphql";
+import client from "../redis/client";
+import { localSpecialtiesKey } from "../redis/keys";
+import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
 
 const getLocalSpecialties: () => Promise<LocalSpecialtyData[]> = async () => {
-  const localSpecialtyRepo = AppDataSource.getRepository(LocalSpecialty);
   try {
-    const localSpecialties: LocalSpecialtyData[] = await localSpecialtyRepo
-      .createQueryBuilder("local_specialty")
-      .innerJoin("local_specialty.regionId", "region")
-      .select([
-        'local_specialty.name AS "localSpecialty"',
-        "region.name AS region",
-        'local_specialty.imageUrl AS "imageUrl"',
-      ])
-      .getRawMany();
-    return localSpecialties;
+    const cachedLocalSpecialties = await client.json.get(localSpecialtiesKey());
+    if (cachedLocalSpecialties) {
+      return cachedLocalSpecialties as LocalSpecialtyData[];
+    } else {
+      const localSpecialtyRepo = AppDataSource.getRepository(LocalSpecialty);
+      const localSpecialties: LocalSpecialtyData[] = await localSpecialtyRepo
+        .createQueryBuilder("local_specialty")
+        .innerJoin("local_specialty.regionId", "region")
+        .select([
+          'local_specialty.name AS "localSpecialty"',
+          "region.name AS region",
+          'local_specialty.imageUrl AS "imageUrl"',
+        ])
+        .getRawMany();
+      await Promise.all([
+        client.json.set(localSpecialtiesKey(), "$", localSpecialties),
+        client.expireAt(localSpecialtiesKey(), expireKeyTomorrow(), "NX"),
+      ]);
+      return localSpecialties;
+    }
   } catch (err) {
     throw new Error("There was an error querying local specialties.");
   }
