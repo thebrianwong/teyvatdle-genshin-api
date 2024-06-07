ef1ce99e71205a199bcb6a8f8c76b34f10be12e5
Brian Wong <brianw7218@gmail.com>
2024-06-06 21:55:37 -0700

    Add caching for talents

--- src/controllers/talentController.ts
+++ src/controllers/talentController.ts
@@ -1,25 +1,37 @@
 import { AppDataSource } from "../index";
 import Talent from "../models/talent.model";
 import { TalentData } from "../generated/graphql";
+import client from "../redis/client";
+import { talentsKey } from "../redis/keys";
+import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
 
 const retrieveTalentData: () => Promise<TalentData[]> = async () => {
-  const talentRepo = AppDataSource.getRepository(Talent);
   try {
-    const talents: TalentData[] = await talentRepo
-      .createQueryBuilder("talent")
-      .innerJoin("talent.characterId", "character")
-      .innerJoin("talent.typeId", "talent_type")
-      .select([
-        'talent.id AS "talentId"',
-        'talent.name AS "talentName"',
-        'talent_type.name AS "talentType"',
-        'talent.imageUrl AS "talentImageUrl"',
-        'character.name AS "characterName"',
-        'character.imageUrl AS "characterImageUrl"',
-      ])
-      .orderBy({ '"talentId"': "ASC" })
-      .getRawMany();
-    return talents;
+    const cachedTalents = await client.json.get(talentsKey());
+    if (cachedTalents) {
+      return cachedTalents as TalentData[];
+    } else {
+      const talentRepo = AppDataSource.getRepository(Talent);
+      const talents: TalentData[] = await talentRepo
+        .createQueryBuilder("talent")
+        .innerJoin("talent.characterId", "character")
+        .innerJoin("talent.typeId", "talent_type")
+        .select([
+          'talent.id AS "talentId"',
+          'talent.name AS "talentName"',
+          'talent_type.name AS "talentType"',
+          'talent.imageUrl AS "talentImageUrl"',
+          'character.name AS "characterName"',
+          'character.imageUrl AS "characterImageUrl"',
+        ])
+        .orderBy({ '"talentId"': "ASC" })
+        .getRawMany();
+      await Promise.all([
+        client.json.set(talentsKey(), "$", talents),
+        client.expireAt(talentsKey(), expireKeyTomorrow(), "NX"),
+      ]);
+      return talents;
+    }
   } catch (err) {
     throw new Error("There was an error querying talents.");
   }
