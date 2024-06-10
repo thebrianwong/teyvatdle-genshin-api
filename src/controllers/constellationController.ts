a601c603af051acbfa4f5de2d39a766d99972af3
Brian Wong <brianw7218@gmail.com>
2024-06-09 22:00:06 -0700

    Convert the constellation cache to use the ioredis client

--- src/controllers/constellationController.ts
+++ src/controllers/constellationController.ts
@@ -7,14 +7,16 @@ import {
   constellationsByCharacterKey,
   constellationsKey,
 } from "../redis/keys";
-import client from "../redis/client";
 import { expireKeyTomorrow } from "../redis/expireKeyTomorrow";
+import { redisClient } from "../redis/redis";
 
 const retrieveConstellationData: () => Promise<
   ConstellationData[]
 > = async () => {
   try {
-    const cachedConstellations = await client.json.get(constellationsKey());
+    const cachedConstellations = await redisClient
+      .call("JSON.GET", constellationsKey())
+      .then((data) => JSON.parse(data as string));
     if (cachedConstellations) {
       return cachedConstellations as ConstellationData[];
     } else {
@@ -33,10 +35,16 @@ const retrieveConstellationData: () => Promise<
         ])
         .orderBy({ '"constellationId"': "ASC" })
         .getRawMany();
-      await Promise.all([
-        client.json.set(constellationsKey(), "$", constellations),
-        client.expireAt(constellationsKey(), expireKeyTomorrow(), "NX"),
-      ]);
+      redisClient
+        .pipeline()
+        .call(
+          "JSON.SET",
+          constellationsKey(),
+          "$",
+          JSON.stringify(constellations)
+        )
+        .expireat(constellationsKey(), expireKeyTomorrow(), "NX")
+        .exec();
       return constellations;
     }
   } catch (err) {
@@ -48,123 +56,130 @@ const retrieveFilteredConstellationData: (
   filterType: "id" | "constellationName" | "characterName",
   searchValue: string
 ) => Promise<ConstellationData[]> = async (filterType, searchValue) => {
-  let constellationCacheKey: string | undefined = searchValue;
-  if (filterType === "constellationName") {
-    constellationCacheKey = await client.hGet(
-      constellationNameToIdKey(),
-      searchValue
-    );
-  }
-
-  let constellationCacheKeyExists;
-  if (filterType === "characterName") {
-    constellationCacheKeyExists = await client.json.type(
-      constellationsByCharacterKey(),
-      constellationCacheKey
-    );
-  } else {
-    constellationCacheKeyExists = await client.json.type(
-      constellationByIdKey(),
-      constellationCacheKey
-    );
-  }
-
-  if (constellationCacheKey && constellationCacheKeyExists) {
-    if (filterType === "characterName") {
-      const constellations = (await client.json.get(
-        constellationsByCharacterKey(),
-        {
-          path: constellationCacheKey,
-        }
-      )) as ConstellationData[];
-      return constellations;
-    } else {
-      const constellations = (await client.json.get(constellationByIdKey(), {
-        path: constellationCacheKey,
-      })) as ConstellationData[];
-      return constellations;
-    }
-  }
   try {
-    const constellationRepo = AppDataSource.getRepository(Constellation);
-    const baseQuery = constellationRepo
-      .createQueryBuilder("constellation")
-      .innerJoin("constellation.characterId", "character")
-      .select([
-        'constellation.id AS "constellationId"',
-        'constellation.name AS "constellationName"',
-        'constellation.level AS "constellationLevel"',
-        'constellation.imageUrl AS "constellationImageUrl"',
-        'constellation.character_id AS "characterId"',
-        'character.name AS "characterName"',
-        'character.imageUrl AS "characterImageUrl"',
-      ]);
-    if (filterType === "id") {
-      baseQuery.where("constellation.id = :id", { id: Number(searchValue) });
-    } else if (filterType === "constellationName") {
-      baseQuery.where("constellation.name = :constellationName", {
-        constellationName: searchValue,
-      });
-    } else if (filterType === "characterName") {
-      baseQuery.where("character.name = :characterName", {
-        characterName: searchValue,
-      });
+    let constellationCacheKey: string | null = searchValue;
+    if (filterType === "constellationName") {
+      constellationCacheKey = await redisClient.hget(
+        constellationNameToIdKey(),
+        searchValue
+      );
     }
-    const constellations: ConstellationData[] = await baseQuery
-      .orderBy({ '"constellationId"': "ASC" })
-      .getRawMany();
-    if (constellations.length > 0) {
-      if (constellations.length === 1) {
-        // id or constellation name search
-        const constellationId = constellations[0].constellationId!.toString();
-        const constellationName = constellations[0].constellationName!;
-        await Promise.all([
-          client.json.set(constellationByIdKey(), "$", {}, { NX: true }),
-          client.json.set(
-            constellationByIdKey(),
-            constellationId,
-            constellations
-          ),
-          { NX: true },
-          client.expireAt(constellationByIdKey(), expireKeyTomorrow(), "NX"),
-          client.hSet(
-            constellationNameToIdKey(),
-            constellationName,
-            constellationId
-          ),
-          client.expireAt(
-            constellationNameToIdKey(),
-            expireKeyTomorrow(),
-            "NX"
-          ),
-        ]);
+
+    let constellationCacheKeyExists;
+    if (constellationCacheKey) {
+      if (filterType === "characterName") {
+        constellationCacheKeyExists = await redisClient.call(
+          "JSON.TYPE",
+          constellationsByCharacterKey(),
+          constellationCacheKey
+        );
       } else {
-        // character name search
-        const characterName = constellations[0].characterName!;
-        await Promise.all([
-          client.json.set(
-            constellationsByCharacterKey(),
-            "$",
-            {},
-            { NX: true }
-          ),
-          client.json.set(
-            constellationsByCharacterKey(),
-            characterName,
-            constellations,
-            {
-              NX: true,
-            }
-          ),
-          client.expireAt(
-            constellationsByCharacterKey(),
-            expireKeyTomorrow(),
-            "NX"
-          ),
-        ]);
+        constellationCacheKeyExists = await redisClient.call(
+          "JSON.TYPE",
+          constellationByIdKey(),
+          constellationCacheKey
+        );
       }
     }
-    return constellations;
+
+    if (constellationCacheKey && constellationCacheKeyExists) {
+      console.log("from cache");
+      if (filterType === "characterName") {
+        const constellations = (await redisClient
+          .call(
+            "JSON.GET",
+            constellationsByCharacterKey(),
+            constellationCacheKey
+          )
+          .then((data) => JSON.parse(data as string))) as ConstellationData[];
+        return constellations;
+      } else {
+        const constellations = (await redisClient
+          .call("JSON.GET", constellationByIdKey(), constellationCacheKey)
+          .then((data) => JSON.parse(data as string))) as ConstellationData[];
+        return constellations;
+      }
+    } else {
+      const constellationRepo = AppDataSource.getRepository(Constellation);
+      const baseQuery = constellationRepo
+        .createQueryBuilder("constellation")
+        .innerJoin("constellation.characterId", "character")
+        .select([
+          'constellation.id AS "constellationId"',
+          'constellation.name AS "constellationName"',
+          'constellation.level AS "constellationLevel"',
+          'constellation.imageUrl AS "constellationImageUrl"',
+          'constellation.character_id AS "characterId"',
+          'character.name AS "characterName"',
+          'character.imageUrl AS "characterImageUrl"',
+        ]);
+      if (filterType === "id") {
+        baseQuery.where("constellation.id = :id", { id: Number(searchValue) });
+      } else if (filterType === "constellationName") {
+        baseQuery.where("constellation.name = :constellationName", {
+          constellationName: searchValue,
+        });
+      } else if (filterType === "characterName") {
+        baseQuery.where("character.name = :characterName", {
+          characterName: searchValue,
+        });
+      }
+      const constellations: ConstellationData[] = await baseQuery
+        .orderBy({ '"constellationId"': "ASC" })
+        .getRawMany();
+      if (constellations.length > 0) {
+        if (constellations.length === 1) {
+          // id or constellation name search
+          const constellationId = constellations[0].constellationId!.toString();
+          const constellationName = constellations[0].constellationName!;
+          redisClient
+            .pipeline()
+            .call(
+              "JSON.SET",
+              constellationByIdKey(),
+              "$",
+              JSON.stringify({}),
+              "NX"
+            )
+            .call(
+              "JSON.SET",
+              constellationByIdKey(),
+              constellationId,
+              JSON.stringify(constellations)
+            )
+            .expireat(constellationByIdKey(), expireKeyTomorrow(), "NX")
+            .hset(
+              constellationNameToIdKey(),
+              constellationName,
+              constellationId
+            )
+            .expireat(constellationNameToIdKey(), expireKeyTomorrow(), "NX")
+            .exec();
+        } else {
+          // character name search
+          const characterName = constellations[0].characterName!;
+          redisClient
+            .pipeline()
+            .call(
+              "JSON.SET",
+              constellationsByCharacterKey(),
+              "$",
+              JSON.stringify({}),
+              "NX"
+            )
+            .call(
+              "JSON.SET",
+              constellationsByCharacterKey(),
+              characterName,
+              JSON.stringify(constellations),
+              "NX"
+            )
+            .expireat(constellationsByCharacterKey(), expireKeyTomorrow(), "NX")
+            .exec();
+        }
+      }
+      return constellations;
+    }
   } catch (err) {
     throw new Error("There was an error querying constellations. " + err);
   }
